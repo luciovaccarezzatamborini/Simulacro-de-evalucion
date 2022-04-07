eaa8dcee5516c733215f1079abe508798dd0ce07
luciovaccarezzatamborini <101400339+luciovaccarezzatamborini@users.noreply.github.com>
2022-04-07 15:47:53 -0300

    Initial commit

---
+++ Assets/Scenes/simulacionej1.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class simulacionej1 : MonoBehaviour
+{
+    public int num1;
+    public int num2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (num1 == num2)
+        {
+            Debug.Log("Los dos numeros ingresados son iguales");
+        }
+        else
+        {
+            Debug.Log("Los numeros ingresados no son iguales");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scenes/simulacroej2.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class simulacroej2 : MonoBehaviour
+{
+
+    public float Producto1;
+    public float Producto2;
+    public float Producto3;
+
+    public float DineroDisponible;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        float suma3;
+        float cuantofalta;
+        float cuantosobra;
+
+        suma3 = Producto1 + Producto2 + Producto3;
+
+        cuantofalta = DineroDisponible - suma3;
+
+        cuantosobra = suma3 - DineroDisponible;
+
+        if (suma3 < DineroDisponible)
+        {
+            Debug.Log("la suma de los 3 productos no supera el dinero disponible y faltan" + cuantofalta + "pesos");
+        }
+        else
+        {
+            Debug.Log("la suma de los 3 productos supera el dinero disponible y sobran " + cuantosobra + " pesos");
+
+        }
+
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+    
+    
